69661a9ed13aae44eb51ce1c9e49be92b3482f89
Elise Dettrick <elise@dettrick.net>
2017-06-19 15:32:25 +1000

    initial commit for problem 02

---
+++ problem_02.py
@@ -0,0 +1,14 @@
+import unittest
+
+def problem_02():
+    pass
+
+class PorjectEulerProblem02Tests(unittest.TestCase):
+    '''By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.'''
+
+    def test_function_runs(self):
+        ''''Basic smole test: does the function run?'''
+        problem_02()
+
+if __name__ == '__main__':
+    unittest.main()
