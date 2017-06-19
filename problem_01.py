233d50e095992456527afda63ad36ec8b5b4a64a
Elise Dettrick <elise@dettrick.net>
2017-06-19 14:28:40 +1000

    adding my first Project Euler project

---
+++ problem_01.py
@@ -0,0 +1,15 @@
+import unittest
+
+def problem_01():
+    pass
+
+class TenpinBowlingScoreTests(unittest.TestCase):
+    '''test for the tenpin bowling score kata'''
+
+    def test_function_runs(self):
+        ''''Basic smole test: does the function run?'''
+        problem_01()
+
+
+if __name__ == '__main__':
+    unittest.main()
