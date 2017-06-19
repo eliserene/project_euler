df4697f582cb24a401c45178684e9e02f8799dfe
Elise Dettrick <elise@dettrick.net>
2017-06-19 16:48:19 +1000

    for numbers less than four million

--- problem_02.py
+++ problem_02.py
@@ -1,28 +1,31 @@
 import unittest
 
-def fibonacci_numbers():
+def fibonacci_numbers(limit):
     previous_term = 1
     yield previous_term
     current_term = 1
     yield current_term
-    while current_term < 80:
+    while current_term < limit:
         next_term = previous_term + current_term
         yield next_term
         previous_term = current_term
         current_term = next_term
 
-def problem_02():
-    return sum(x for x in fibonacci_numbers() if x % 2 == 0)
+def problem_02(limit):
+    return sum(x for x in fibonacci_numbers(limit) if x % 2 == 0)
 
 class PorjectEulerProblem02Tests(unittest.TestCase):
     '''By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.'''
 
     def test_function_runs(self):
         ''''Basic smole test: does the function run?'''
-        problem_02()
+        problem_02(80)
 
     def test_the_first_ten_terms(self):
-        self.assertEqual(problem_02(), 44)
+        self.assertEqual(problem_02(80), 44)
+
+    def test_less_than_four_million(self):
+        self.assertEqual(problem_02(4000000), 4613732)
 
 if __name__ == '__main__':
     unittest.main()
