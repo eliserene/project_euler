479e0096a719d00f13d8fe161b26cad4b6bf12f4
Elise Dettrick <elise@dettrick.net>
2017-06-19 14:55:23 +1000

    adding a test for multiples less than 10

--- problem_01.py
+++ problem_01.py
@@ -1,15 +1,19 @@
 import unittest
+from functools import reduce
 
 def problem_01():
-    pass
+    numbers = [x for x in range(10) if x % 3 == 0 or x % 5 == 0]
+    return reduce((lambda x, y: x + y), numbers, 0)
 
-class TenpinBowlingScoreTests(unittest.TestCase):
+class PorjectEulerProblem01Tests(unittest.TestCase):
     '''test for the tenpin bowling score kata'''
 
     def test_function_runs(self):
         ''''Basic smole test: does the function run?'''
         problem_01()
 
+    def test_multiples_below_10(self):
+        self.assertEqual(problem_01(), 23)
 
 if __name__ == '__main__':
     unittest.main()
