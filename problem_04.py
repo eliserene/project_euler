01b9132a70adc1336b872071f4132499305d5672
Elise Dettrick <elise@dettrick.net>
2017-06-20 10:52:05 +1000

    adding is_palindrome function

--- problem_04.py
+++ problem_04.py
@@ -1,5 +1,8 @@
 import unittest
 
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
 def problem_04():
     pass
 
@@ -9,6 +12,10 @@ class PorjectEulerProblem04Tests(unittest.TestCase):
     def test_function_runs(self):
         ''''Basic smole test: does the function run?'''
         problem_04()
+
+    def test_is_pelindrome(self):
+        self.assertTrue(is_palindrome(121))
+        self.assertFalse(is_palindrome(123))
 
 if __name__ == '__main__':
     unittest.main()
