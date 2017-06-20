dd23b9c7ccae635c110ed91d3855705fe12cf2f3
Elise Dettrick <elise@dettrick.net>
2017-06-20 14:17:55 +1000

    created a Factorial class

--- problem_20.py
+++ problem_20.py
@@ -1,14 +1,19 @@
 import unittest
 
-def problem_20():
-    pass
+class Factorial:
+
+    def sum_digits(self):
+        pass
 
 class PorjectEulerProblem20Tests(unittest.TestCase):
     '''Find the largest palindrome made from the product of two 3-digit numbers.'''
 
+    def setUp(self):
+        self.f = Factorial()
+
     def test_function_runs(self):
         ''''Basic smole test: does the function run?'''
-        problem_20()
+        self.f.sum_digits()
 
 if __name__ == '__main__':
     unittest.main()
