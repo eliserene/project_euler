ad81fbada605caccc3d87bd271f1be18ffec7ced
Elise Dettrick <elise@dettrick.net>
2017-06-20 10:00:19 +1000

    initial commit for problem 04

---
+++ problem_04.py
@@ -0,0 +1,14 @@
+import unittest
+
+def problem_04():
+    pass
+
+class PorjectEulerProblem04Tests(unittest.TestCase):
+    '''Find the largest palindrome made from the product of two 3-digit numbers.'''
+
+    def test_function_runs(self):
+        ''''Basic smole test: does the function run?'''
+        problem_04()
+
+if __name__ == '__main__':
+    unittest.main()
