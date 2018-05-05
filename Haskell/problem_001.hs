6f8923d07a5c726cf73ded72e05f77a86fb803ee
Elise Dettrick <elise@dettrick.net>
2018-05-05 22:50:18 +1000

    Add solution for problem 1 in Haskell

---
+++ Haskell/problem_001.hs
@@ -0,0 +1,16 @@
+--Problem 001
+--Name: Multiples of 3 and 5
+--URL: https://projecteuler.net/problem=1
+--Text: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+--Find the sum of all the multiples of 3 or 5 below 1000.
+
+import Test.HUnit 
+
+problem001 n = sum [x | x <- [1..(n - 1)], x `mod` 3 == 0 || x `mod` 5 == 0]
+
+main = putStrLn $ show (problem001 1000)
+
+test1 = TestCase $ assertEqual "less than 10" 23 (problem001 10)
+test2 = TestCase $ assertEqual "less than 100" 2318 (problem001 100)
+
+tests = TestList [TestLabel "test1: less than 10" test1, TestLabel "test 2: less than 100" test2]
